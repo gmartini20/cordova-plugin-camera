9e18febdce4765028ae2fe4c741160b19d32437f
Guilherme Martini <guilherme.martini20@gmail.com>
2017-05-15 13:35:20 -0300

    added missing ;

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -163,7 +163,7 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
             this.allowEdit = args.getBoolean(7);
             this.correctOrientation = args.getBoolean(8);
             this.saveToPhotoAlbum = args.getBoolean(9);
-            this.getFromPhotoApps = args.getBoolean(10)
+            this.getFromPhotoApps = args.getBoolean(10);
 
             // If the user specifies a 0 or smaller width/height
             // make it -1 so later comparisons succeed
